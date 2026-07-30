436cfbd5167f89586787464978d852f9bd9d0886
Sergio Quiñones <TU_CORREO_DE_GITHUB>
2026-07-30 12:57:38 -0500

    Primer proyecto: Gestor de tareas en Python

---
+++ Gestor_Tareas.py
@@ -0,0 +1,59 @@
+tareas = []
+
+while True:
+    print("\n===== GESTOR DE TAREAS =====")
+    print("1. Ver tareas")
+    print("2. Agregar tareas")
+    print("3. Marcar tarea")
+    print("4. Eliminar tarea")
+    print("5. Salir")
+
+    opcion = input("Seleccione una opción: ")
+    if opcion == "1":
+        print("\n ===== MIS TAREAS =====")
+        if len(tareas) == 0:
+            print("No hay tareas registradas.")
+        else:
+            for tarea in tareas:
+                print(tarea)
+    elif opcion == "2":
+        tarea = input("Escriba la nueva tarea: ")
+        tareas.append(tarea)
+        print("Tarea agregada con exito.")
+    elif opcion == "3":
+        print("\n===== MARCAR TAREA COMO COMPLETADA =====")
+
+        if len(tareas) == 0:
+            print("No hay tareas registradas.")
+
+        else:
+            for numero, tarea in enumerate(tareas, 1):
+                print(numero, tarea)
+            numero = input("Ingrese la tarea completada ")
+            completada = int(numero)
+            tareas[completada - 1] = "COMPLETADA " + tareas[completada - 1]
+            print("Tarea marcada exitosamente. ")
+        
+    elif opcion =="4":
+        print("Que tarea deseas eliminar")
+        for numero, tarea in enumerate(tareas):
+            print(numero, tarea)
+        numero = input("Ingrese la tarea que desea eliminar ")
+        numeros = int(numero)
+        tareas.pop(numeros)
+        print("Tarea eliminada con éxito")
+    elif opcion == "5":
+        guardar = input("¿Desea guardar las tareas antes de salir? (s/n): ")
+
+        if guardar.lower() == "s":
+            archivo = open("tareas.txt", "w")
+            for tarea in tareas:
+                archivo.write(tarea + "\n")
+            archivo.close()
+
+        print("Tareas guardadas correctamente.")
+
+        print("¡Hasta luego!")
+        break
+    else:
+        print("Esta opción aún no esta disponible")
